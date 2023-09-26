9b589ba20104ced908d4df8cebf019b188b933d9
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2023-09-26 23:18:49 +0300

    added header__middle

---
+++ src/components/Search.vue
@@ -0,0 +1,66 @@
+<script setup>
+import SearchIcon from './icons/SearchIcon.vue'
+</script>
+
+<template>
+  <div class="search">
+    <form class="search__form">
+      <input class="search__input" type="search" placeholder="Поиск">
+      <button class="search__btn">
+        <SearchIcon />
+      </button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.search {
+  max-width: 444px;
+  width: 100%;
+}
+
+.search__form {
+  position: relative;
+  max-width: 100%;
+  width: 100%;
+  height: 48px;
+}
+
+.search__input {
+  padding: 16px 50px 16px 12px;
+  max-width: 100%;
+  width: 100%;
+  height: 48px;
+  border: none;
+  border-bottom: 1px solid #D1D1D3;
+  background-color: #f6f6f6;
+  font-family: 'Manrope', sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 136%;
+  color: #8B8D92;
+}
+
+.search__input::placeholder {
+  font-family: 'Manrope', sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 136%;
+  color: #8B8D92;
+}
+
+.search__btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.search__btn {
+  width: 48px;
+  height: 48px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/UserNav.vue
@@ -0,0 +1,69 @@
+<script setup>
+import ProfileIcon from './icons/ProfileIcon.vue'
+import OrdersIcon from './icons/OrdersIcon.vue'
+import CartIcon from './icons/CartIcon.vue'
+</script>
+
+<template>
+  <nav class="user-nav">
+    <ul class="user-nav__list list-reset">
+      <li class="user-nav__item">
+        <router-link class="user-nav__link" to="/profile/">
+          <ProfileIcon />
+          <span>Профиль</span>
+        </router-link>
+      </li>
+
+      <li class="user-nav__item">
+        <router-link class="user-nav__link" to="/orders/">
+          <OrdersIcon />
+          <span>Заказы</span>
+        </router-link>
+      </li>
+
+      <li class="user-nav__item">
+        <router-link class="user-nav__link" to="/cart/">
+          <CartIcon />
+          <span>Корзина</span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.user-nav {
+  max-width: 246px;
+  width: 100%;
+}
+
+.user-nav__list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.user-nav__link {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: opacity 0.2s ease-in-out;
+}
+
+.user-nav__link:hover {
+  opacity: 0.7;
+}
+
+.user-nav__link:active {
+  opacity: 0.5;
+}
+
+.user-nav__link span {
+  font-family: Manrope;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 145%;
+  color: #454950;
+}
+</style>
